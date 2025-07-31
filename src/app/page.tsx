19f354899fa6fe2112916ae425581404bcd6c59d
Александр Гришин <grishin.sasha2001@gmail.com>
2025-07-31 07:00:26 +0000

    Для планшетных и мобильных версий нужно полностью убрать отступы по бока

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import YearlyAbsenceTracker from '@/components/yearly-absence-tracker';
 export default function Home() {
   return (
     <div className="bg-background min-h-screen">
-      <div className="container mx-auto p-4 sm:p-6 lg:p-8">
+      <div className="container mx-auto px-0 sm:px-0 lg:px-8">
         <YearlyAbsenceTracker />
       </div>
     </div>
