a7970875322c3506ba15ce76a30ac09892ac2da9
Александр Гришин <grishin.sasha2001@gmail.com>
2025-07-31 07:03:08 +0000

    Давай уберём отступы на странице вообще. Чтобы таблица была на всю ширин

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,7 @@ import YearlyAbsenceTracker from '@/components/yearly-absence-tracker';
 export default function Home() {
   return (
     <div className="bg-background min-h-screen">
-      <div className="container mx-auto px-0 sm:px-0 lg:px-8">
-        <YearlyAbsenceTracker />
-      </div>
+      <YearlyAbsenceTracker />
     </div>
   );
 }
